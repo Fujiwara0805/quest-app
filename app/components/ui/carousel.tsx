ddfb8543e92f60716e70cf9fbc3abe2e951bcd22
Fujiwara0805 <tiki4091@gmail.com>
2025-02-08 21:01:25 +0900

    ✨ Create Carousel component with Embla Carousel types

---
+++ app/components/ui/carousel.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import * as React from 'react';
+import useEmblaCarousel from 'embla-carousel-react';
+import { ArrowLeft, ArrowRight } from 'lucide-react';
+
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+
+type CarouselApi = ReturnType<typeof useEmblaCarousel>[1];
+type UseCarouselParameters = Parameters<typeof useEmblaCarousel>;
+type CarouselOptions = UseCarouselParameters[0];
+type CarouselPlugin = UseCarouselParameters[1];
+
+// 以下、残りのコード... 
