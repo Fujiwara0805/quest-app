ba577df2f30d8d5b19a818aa1ed3972d1c9fbea4
Fujiwara0805 <tiki4091@gmail.com>
2025-02-08 20:50:45 +0900

    ✨ Add Radix UI Avatar component

--- global.d.ts
+++ global.d.ts
@@ -1,2 +1,4 @@
 // 型定義が存在しない場合の対応として、モジュール宣言を行います。
-declare module '@radix-ui/react-alert-dialog'; 
+declare module '@radix-ui/react-alert-dialog';
+declare module '@radix-ui/react-aspect-ratio';
+declare module '@radix-ui/react-avatar'; 
