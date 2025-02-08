e9ca4307fcaf3545c0c078641265e31c94da6250
Fujiwara0805 <tiki4091@gmail.com>
2025-02-08 21:32:43 +0900

    ✨ Add Radix UI Checkbox component

--- app/components/ui/carousel.tsx
+++ app/components/ui/carousel.tsx
@@ -1,15 +1,8 @@
 'use client';
 
-import * as React from 'react';
 import useEmblaCarousel from 'embla-carousel-react';
-import { ArrowLeft, ArrowRight } from 'lucide-react';
-
-import { cn } from '@/lib/utils';
-import { Button } from '@/components/ui/button';
 
 type CarouselApi = ReturnType<typeof useEmblaCarousel>[1];
 type UseCarouselParameters = Parameters<typeof useEmblaCarousel>;
 type CarouselOptions = UseCarouselParameters[0];
 type CarouselPlugin = UseCarouselParameters[1];
-
-// Carouselコンポーネントの実装（以下に実際のコードを記述）
