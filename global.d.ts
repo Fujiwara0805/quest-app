8867b0456d79cc14d0e9650fd5e0105ac0992476
Fujiwara0805 <tiki4091@gmail.com>
2025-02-08 20:55:17 +0900

    ✨ Add Embla Carousel type declaration and dependency

--- global.d.ts
+++ global.d.ts
@@ -3,4 +3,7 @@ declare module '@radix-ui/react-alert-dialog';
 declare module '@radix-ui/react-aspect-ratio';
 declare module '@radix-ui/react-avatar';
 // react-day-picker の型定義が存在しない場合に備えて、モジュール宣言を追加
-declare module 'react-day-picker'; 
+declare module 'react-day-picker';
+
+// embla-carousel-react の型定義が存在しない場合に備えて、モジュール宣言を追加
+declare module 'embla-carousel-react'; 
