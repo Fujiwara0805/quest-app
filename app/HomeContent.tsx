fb859f02fe8e339fbcac90e9b03f2d8f2078a97d
Fujiwara0805 <tiki4091@gmail.com>
2025-02-10 17:53:43 +0900

    🔥 Remove unused HomeContent component

--- app/HomeContent.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useSearchParams } from "next/navigation";
-
-export default function HomeContent() {
-  const searchParams = useSearchParams();
-  // searchParams を利用した処理を記述
-  return <div>Home Content</div>;
-} 
