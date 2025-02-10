87269a4244087d4346df738570b51f6a49778691
Fujiwara0805 <tiki4091@gmail.com>
2025-02-10 18:17:18 +0900

    🔧 Remove dynamic import for QuestListContainer

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,14 +1,7 @@
 // 以前は「use client」ディレクティブが記述されていたかもしれませんが、サーバーコンポーネントとして扱うために削除します。
 
 import React, { Suspense } from "react";
-import dynamic from "next/dynamic";
-
-// クライアントコンポーネントを dynamic() 経由でインポートし、suspense オプションとともに
-// ssr: false を指定して、サーバー側でのレンダリングを無効にします
-const QuestListContainer = dynamic(() => import("../components/QuestListContainer"), {
-  suspense: true,
-  ssr: false,
-});
+import QuestListContainer from "../components/QuestListContainer";
 
 export default function MainPage() {
   return (
