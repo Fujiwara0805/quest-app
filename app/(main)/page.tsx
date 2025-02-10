91b4ac67a2378110391b1fa0ec7d1d64b257ddd0
Fujiwara0805 <tiki4091@gmail.com>
2025-02-10 18:13:56 +0900

    🔧 Disable SSR for QuestListContainer dynamic import

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -3,9 +3,11 @@
 import React, { Suspense } from "react";
 import dynamic from "next/dynamic";
 
-// クライアントコンポーネントを dynamic() 経由でインポートし、suspense オプションを有効にします
+// クライアントコンポーネントを dynamic() 経由でインポートし、suspense オプションとともに
+// ssr: false を指定して、サーバー側でのレンダリングを無効にします
 const QuestListContainer = dynamic(() => import("../components/QuestListContainer"), {
   suspense: true,
+  ssr: false,
 });
 
 export default function MainPage() {
