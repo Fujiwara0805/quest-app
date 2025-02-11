3abf101d5b12a4ab304d5e1a9fdb118469a6dff7
Fujiwara0805 <tiki4091@gmail.com>
2025-02-11 22:09:38 +0900

    🗑️ Remove multiple Supabase migration files for test user setup

--- supabase/migrations/20250112123032_rough_unit.sql
+++
@@ -1,9 +0,0 @@
--- Set password for test user
-UPDATE auth.users
-SET encrypted_password = crypt('testuser123', gen_salt('bf'))
-WHERE email = 'test.user@example.com';
-
--- Set password for test quest master
-UPDATE auth.users
-SET encrypted_password = crypt('testmaster123', gen_salt('bf'))
-WHERE email = 'test.master@example.com';
